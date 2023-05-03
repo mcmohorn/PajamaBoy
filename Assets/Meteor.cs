a0f2b9d87910733df08a225448a073b747af3402
Matt Mohorn <mcmohorn@gmail.com>
2023-05-03 15:52:51 -0600

    Meteor scene explosion

---
+++ Assets/Meteor.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Meteor : MonoBehaviour
+{
+
+    Rigidbody rb;
+    public Rigidbody rockBody;
+    public float rockRotation;
+    Vector3 dir;
+    public ParticleSystem explosionPS;
+    public ParticleSystem explosionPS2;
+
+    bool moving = true;
+
+    public float delayUntilExplosion;
+    public float delayUntilExplosion2;
+
+    public float delayUntilNextScene;
+
+    public string nextScene;
+
+    void Start()
+    {
+        dir = new Vector3(0,0,10);
+        rb = GetComponent<Rigidbody>();
+
+        Invoke("Explode", delayUntilExplosion);
+        Invoke("Explode2", delayUntilExplosion2);
+        Invoke("NextScene", delayUntilNextScene);
+    }
+
+    void Update()
+    {
+        rb.AddForce(dir);
+        rockBody.AddForce(dir);
+        rockBody.AddTorque(transform.up * rockRotation);
+        
+    }
+
+    void Explode() 
+    {
+        explosionPS.Play();
+        Destroy(explosionPS, explosionPS.duration);
+    }
+
+    void Explode2() 
+    {
+        explosionPS2.Play();
+        Destroy(explosionPS2, explosionPS2.duration);
+    }
+
+    void NextScene()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+}
